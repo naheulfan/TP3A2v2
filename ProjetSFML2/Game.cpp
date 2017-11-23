7cd53228f47ce294f8a3c3d4a287f97dcfdda202
naheulfan <brunopleau@hotmail.com>
2017-11-23 14:06:52 -0500

    FIRST COMMIT

---
+++ ProjetSFML2/Game.cpp
@@ -0,0 +1,87 @@
+#include "Game.h"
+
+Game::Game()
+{
+	//On place dans le contructeur ce qui permet à la game elle-même de fonctionner
+
+	mainWin.create(VideoMode(LARGEUR, HAUTEUR, 32), "Le titre de mon jeu");  // , Style::Titlebar); / , Style::FullScreen);
+	view = mainWin.getDefaultView();
+
+	//Synchonisation coordonnée à l'écran!  Normalement 60 frames par secondes. À faire absolument
+	mainWin.setVerticalSyncEnabled(true);
+	//mainWin.setFramerateLimit(60);  //Équivalent... normalement, mais pas toujours. À utiliser si la synchonisation de l'écran fonctionne mal.
+	//https://www.sfml-dev.org/tutorials/2.0/window-window.php#controlling-the-framerate
+}
+
+int Game::testTest()
+{
+	return 0;
+}
+
+int Game::run()
+{
+	if (!init())
+	{
+		return EXIT_FAILURE;
+	}
+
+	while (mainWin.isOpen())
+	{
+		getInputs();
+		update();
+		draw();
+	}
+
+	return EXIT_SUCCESS;
+}
+
+bool Game::init()
+{
+	backgroundT.loadFromFile("Ressources\\starfieldSprite.png");
+	background[0].setTexture(backgroundT);
+	background[0].setPosition(0, 0);
+	background[1].setTexture(backgroundT);
+	background[1].setPosition(2560, 0);
+	return true;
+}
+
+void Game::getInputs()
+{
+	//On passe l'événement en référence et celui-ci est chargé du dernier événement reçu!
+	while (mainWin.pollEvent(event))
+	{
+		//x sur la fenêtre
+		if (event.type == Event::Closed)
+		{
+			mainWin.close();
+		}
+	}
+}
+
+void Game::update()
+{
+#pragma region BackgroundUpdates
+	background[0].setPosition(Vector2f(background[0].getPosition().x - 2, background[0].getPosition().y));
+	background[1].setPosition(Vector2f(background[1].getPosition().x - 2, background[1].getPosition().y));
+	if (background[0].getPosition().x <= -1280 && (background[1].getPosition().x > 1280 || background[1].getPosition().x <= -1280))
+	{
+		background[1].setPosition(Vector2f(1280, 0));
+
+	}
+	else if (background[0].getPosition().x <= -2560)
+	{
+		background[0].setPosition(1280, 0);
+	}
+#pragma endregion BackgroundUpdates
+
+
+}
+
+void Game::draw()
+{
+	//Toujours important d'effacer l'écran précédent
+	mainWin.clear();
+	mainWin.draw(background[0]);
+	mainWin.draw(background[1]);
+	mainWin.display();
+}
