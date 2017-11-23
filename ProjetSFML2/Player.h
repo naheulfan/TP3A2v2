362080d00cc470c9ffddb1c80d423bd934571a54
naheulfan <brunopleau@hotmail.com>
2017-11-23 14:21:31 -0500

    Push test

--- ProjetSFML2/Player.h
+++ ProjetSFML2/Player.h
@@ -1,8 +1,15 @@
 #pragma once
+#include <SFML\Graphics.hpp>
+using namespace sf;
 class Player
 {
 public:
 	Player();
 	~Player();
+	void Init(Texture playerTexture);
+	void Update();
+	void Draw();
+private:
+	Vector2f position;
 };
 
